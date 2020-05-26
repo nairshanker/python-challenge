a88ac18a2e261ce10c404288b9dd27bd2ac8caf7
nairshanker <nair.shanker@gmail.com>
2020-05-25 20:22:40 -0500

    pushing pybank HW

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,23 @@
 import os
 import csv
 
-# Read the csv file
-csvpath = os.path.join(Resources", "budget_data.csv")
 
-# Read the csv
+# Read the csv file
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Read the csv file 
 with open(csvpath) as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=",")
-        next(csvreader)
+        csvreader = csv.reader(csvfile, delimiter=',')
+        print(csvreader)
+
+        csv_header = next(csvreader)
+        print(f"CSV Header: {csv_header}")
+
+        row_count = 0
+        for row in csvreader:
+                row_count = row_count + 1
+                # print(row)
+        print("Financial Analysis")
+        print("----------------------------")
+        print(f"Total Months: {row_count}")
+
