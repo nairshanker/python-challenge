c8515f67ba75bb7d99da2bd7f0b2793b9c759c19
nairshanker <nair.shanker@gmail.com>
2020-05-27 18:58:07 -0500

    pushing pybank homework progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,46 +6,51 @@ import csv
 # Specify the csv file path with aa relative reference
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+# Initialize values 
+month_count = 0
+net_p_and_l = 0
+net_delta_p_and_l = []
+greatest_increase = ["",0]
+greatest_decrease = ["",10E10]
+
 # Read the csv file with the improved method using csv reader.
 # csv reader method is an iterator. It creates a marker to iterate through the rows and also csv reader creates a list of rows.
-
 with open(csvpath) as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
         print(csvreader)
 
-# Skip the header since we are trying to count the number of months in the actual data 
+# Accessing the header row 
         csv_header = next(csvreader)
 
-# Initialize row counter adn net_Profile/Loss to zero
-        row_count = 0
-        net_p_and_l = 0
-        # net_p_and_l = []
-        delta_p_and_l = 0
-        sigma_delta_p_and_l = 0
-        # average_change = []
+# Skip the header since we are trying to count the number of months in the actual data 
+        first_row = next(csvreader)
+        month_count += 1
+        net_p_and_l += int(first_row[1])
+
+# Initialize the first row            
+        previous_row = int(first_row[1])
 
 
 # csv module has the row option and allows one to iterate row by row
         for row in csvreader:
-                row_count = row_count + 1
-                # net_p_and_l.append(row[1])
+                month_count = month_count + 1
                 net_p_and_l += int(row[1])
-                delta_p_and_l = int(row[1])-delta_p_and_l
-                # sigma_delta_p_and_l += delta_p_and_l
-                
-        # Calculating the average change on P/L after the rows and net P/l have been counted separately
-        # Average_change = delta_p_and_l/(row_count-1)
-        # total = sum(int(net_p_and_l))
 
-
-        print("Financial Analysis\n")
-        print("----------------------------\n")
-        print(f"Total Months: {row_count}\n")
-        print(f"Total: ${net_p_and_l}\n")
-        print(f"{delta_p_and_l}")
-        # print(f"Total: ${str(total)}")
-        # print(net_p_and_l)
-
+                delta_p_and_l = int(row[1])-previous_row
+                previous_row = int(row[1])
+                net_delta_p_and_l += [delta_p_and_l]
         
-        # print(f"Average Change: ${delta_p_and_l}")
+        if delta_p_and_l > greatest_increase[1]:
+                
+        
+average = round(sum(net_delta_p_and_l)/len(net_delta_p_and_l),2)
+
+                      
+
+print("Financial Analysis\n")
+print("------------------------\n")
+print(f"Total Months: {month_count}\n")
+print(f"Total: {net_p_and_l}\n")
+print(f"Average  Change: ${average}\n")
+
 
